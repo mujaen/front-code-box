e04341204124047a1eb753b463893947dff1db9d
mujaen <mujaen@nate.com>
2023-03-18 18:08:28 +0900

    QUERY TEST

---
+++ react-query/src/pages/_app.tsx
@@ -0,0 +1,33 @@
+import '@/styles/globals.css'
+import type { AppProps } from 'next/app'
+
+import axios from 'axios'
+
+import { QueryClient, QueryClientProvider} from '@tanstack/react-query'
+
+export default function App({ Component, pageProps }: AppProps) {
+  const defaultQueryFn = async ({ queryKey }) => {
+    const { data } = await axios.get(
+      `https://jsonplaceholder.typicode.com${queryKey[0]}`,
+    )
+    return data
+  }
+
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        queryFn: defaultQueryFn,
+        retry: false,
+        refetchOnReconnect: false,
+        refetchOnMount: false,
+        refetchOnWindowFocus: false,
+      },
+    },
+  })
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  )
+}
